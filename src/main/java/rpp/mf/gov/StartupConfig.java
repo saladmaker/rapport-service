a4440db3a0149fc9be6d0c10db6b59ea73beae56
saladmaker <khaledabderrahim512@gmail.com>
2025-08-17 16:33:52 +0200

    initial commit tbc

--- src/main/java/rpp/mf/gov/StartupConfig.java
+++ src/main/java/rpp/mf/gov/StartupConfig.java
@@ -3,17 +3,14 @@ package rpp.mf.gov;
 import java.util.Set;
 
 import io.quarkus.runtime.StartupEvent;
-import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
 
-@ApplicationScoped
-public class RegisterDefaultUser {
-
+public class StartupConfig {
     @Inject
     UserRepo repo;
 
-    public void onStartUp(@Observes StartupEvent event){
+    public void onStartUp(@Observes StartupEvent event) {
         repo.save(new User("khaled", "df", "1994", Set.of("admin", "user")));
     }
 }
